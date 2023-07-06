5f44ff0c814399f0ee1214e3a03d3511374f53b0
Harunobu Ishii <harunobuishii547@gmail.com>
2023-07-06 17:05:13 -0400

    DB setup

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CollectionModule } from './collection/collection.module'
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), CollectionModule, GraphQLModule.forRoot({
+    autoSchemaFile: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
