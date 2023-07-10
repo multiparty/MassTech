56f34d85b853714c5fcd6dd08c090568aedddeb8
Akosah <emmanuel.s.akosah.19@gmail.com>
2023-07-10 15:13:51 -0400

    remove dist

--- packages/server/dist/participant/participant.pipe.d.ts
+++
@@ -1,8 +0,0 @@
-import { PipeTransform } from '@nestjs/common';
-import { Participant } from './participant.model';
-import { ParticipantService } from './participant.service';
-export declare class ParticipantPipe implements PipeTransform<string, Promise<Participant>> {
-    private readonly participantService;
-    constructor(participantService: ParticipantService);
-    transform(value: string): Promise<Participant>;
-}
