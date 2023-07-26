221ed6fbc6221806e7dbf10244c317431117ad65
Snafkin547 <62607343+Snafkin547@users.noreply.github.com>
2023-07-26 13:41:14 -0400

    collection pagination (#5)
    * collection pagination
    
    * collection pagination
    
    * port config changed docker
    
    * Returned data type added in pagination collection class
    
    * Fixed bugs
    
    * pagination

---
+++ packages/server/src/collection/collection.pageinfo.ts
@@ -0,0 +1,39 @@
+import { Field, ObjectType, InputType } from '@nestjs/graphql'
+import { Collection } from './collection.model';
+
+@ObjectType()
+export class PageInfo {
+
+  @Field()
+  public pageSize: number; //size of page
+
+  @Field()
+  public page: number; //current page
+
+  @Field()
+  public total: number; //total number of documents
+
+  @Field()
+  public hasNext: boolean // true if next page
+
+  @Field()
+  public hasPrev: boolean // true if has previous page
+}
+
+@InputType()
+export class PageInfoInput {
+  @Field()
+  public page: number = 0;
+
+  @Field()
+  public pageSize: number = 10; //size of page
+}
+
+@ObjectType()
+export class PaginatedCollections {
+  @Field(() => [Collection])
+  public data: Collection[]
+
+  @Field(() => PageInfo)
+  public pageInfo: PageInfo
+}
--- packages/server/src/collection/collection.service.ts
+++ packages/server/src/collection/collection.service.ts
@@ -3,18 +3,43 @@ import { InjectModel } from '@nestjs/mongoose';
 import mongoose, { Model } from 'mongoose';
 import { Collection, CollectionDocument } from './collection.model'
 import { CollectionType } from './collection.type'
+import { PageInfo, PageInfoInput, PaginatedCollections} from './collection.pageinfo'
 
 @Injectable()
 export class CollectionService {
     constructor(@InjectModel(Collection.name) private collectionModel: Model<CollectionDocument>) {}
-    
+
     async create(input:CollectionType):Promise<Collection>{
         const createdCollection = new this.collectionModel(input);
         return createdCollection.save();
     }
 
-    async findAll(): Promise<Collection[]>{
-        return this.collectionModel.find({ deletedAt: null }).exec()
+    async findAll(input: PageInfoInput): Promise<PaginatedCollections> {
+        const { page, pageSize } = input;
+        const skipCount = (page - 1) * pageSize;
+        const collections = await this.collectionModel
+            .find({ deletedAt: null })
+            .skip(skipCount)
+            .limit(pageSize)
+            .exec();
+
+
+        const startIndex = (page - 1) * pageSize
+        const endIndex = page * pageSize;
+
+        const pageInfo: PageInfo = {
+            pageSize: pageSize,
+            page: page,
+            total: collections.length,
+            hasNext: endIndex < collections.length,
+            hasPrev: startIndex > 0,
+        };
+
+        const paginatedCollection:PaginatedCollections = {
+            data:collections,
+            pageInfo: pageInfo
+        }
+        return paginatedCollection;
     }
 
     async find(id: string): Promise<Collection|null>{
